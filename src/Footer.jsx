fa71f0da9df43cfd32fb29b40c90cf7c2f3aeb2f
stella4444 <stella42044@gmail.com>
2024-12-20 22:56:29 -0500

    finished commit

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Stella's Photography</p>
+        </footer>
+    );
+}
+
+export default Footer
